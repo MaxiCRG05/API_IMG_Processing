312638e7a9210b7bc68668391fc0a473f6cd41d6
MaxiCRG05 <maxicrg.05@gmail.com>
2025-07-07 17:33:09 -0600

    5 interfaces ya realizadas

---
+++ WebService/Controllers/WEB/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebService.Controllers.WEB
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar()
+		{
+			return View();
+		}
+        
+        public ActionResult Modificar()
+		{
+			return View();
+		}
+	}
+}
